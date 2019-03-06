fd315010ade074834573e7f4a49592086c3a32aa
Git7ob <hamza@sc0rp.eduroam.wireless.dtu.dk>
2019-03-06 14:33:18 +0100

    last Modification

---
+++ hdmi-in.sdk/HDMI_IN/Debug/src/platform/platform.d
@@ -0,0 +1,17 @@
+src/platform/platform.o src/platform/platform.o: \
+ ../src/platform/platform.c \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform/platform_config.h
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform/platform_config.h:
---
+++ hdmi-in.sdk/HDMI_IN/Debug/src/video_demo.d
@@ -0,0 +1,139 @@
+src/video_demo.o src/video_demo.o: ../src/video_demo.c \
+ ../src/video_demo.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h \
+ ../src/display_ctrl/display_ctrl.h ../src/display_ctrl/vga_modes.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma_hw.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma_i.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xdebug.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xvtc.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xvtc_hw.h \
+ ../src/display_ctrl/../dynclk/dynclk.h ../src/intc/intc.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xscugic.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_exception.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xuartps.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h \
+ ../src/timer_ps/timer_ps.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_exception.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xsdps.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xsdps_hw.h \
+ ../src/BTNInterrupt/interrupts.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xgpio.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/xtime_l.h \
+ ../src/sprites/sprites.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/ff.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/integer.h \
+ ../../standalone_bsp_1/ps7_cortexa9_0/include/ffconf.h \
+ ../src/platform/platform.h ../src/platform/platform_config.h
+
+../src/video_demo.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h:
+
+../src/display_ctrl/display_ctrl.h:
+
+../src/display_ctrl/vga_modes.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma_hw.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xaxivdma_i.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xdebug.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xvtc.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xvtc_hw.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+../src/intc/intc.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xscugic.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_exception.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xuartps.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h:
+
+../src/timer_ps/timer_ps.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_exception.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xsdps.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_cache.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xsdps_hw.h:
+
+../src/BTNInterrupt/interrupts.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xgpio.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/xtime_l.h:
+
+../src/sprites/sprites.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/ff.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/integer.h:
+
+../../standalone_bsp_1/ps7_cortexa9_0/include/ffconf.h:
+
+../src/platform/platform.h:
+
+../src/platform/platform_config.h:
